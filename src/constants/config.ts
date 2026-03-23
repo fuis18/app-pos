4cff98a532f5ffc7f29b548a4e6e46bd7ecff1c8
fuis18 <warfuis18larcz018@gmail.com>
2026-03-23 17:16:23 -0500

    Update the token

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
 	LIMIT: 10,
-	TOKEN: "123456",
+	TOKEN: "182502",
 	GITHUB_PROFILE_URL: "https://github.com/Fuis18/",
 	GITHUB_REPO_URL: "https://github.com/fuis18/restaurant-pos",
 };
